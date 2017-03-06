5566122e07c5809cb89eab497f0d50ee34cb5efb
Crazy_Cactus <Mathias.Genesis@Gmail.com>
2017-03-06 15:16:06 +0100

    colors test tab

--- ex1/colors.py
+++ ex1/colors.py
@@ -1,12 +1,20 @@
-from ev3dev import *
+from ev3dev.ev3 import *
+
+ev3.Sound.setLanguage("French")
 
 color_sensor = ev3.ColorSensor()
 color_sensor.mode = 'COL-COLOR'
 
+colors = ["What the fuck", "noir", "bleu", "vert", "jaune", "alalalal", "blanc", "marron"]
+
 while True:
     print(color_sensor.value())
-    motor = ev3.LargeMotor('outA')
-    motor.run_timed(time_sp=3000, speed_sp=500)
-    
+    #motor = ev3.LargeMotor('outA')
+    #motor.run_timed(time_sp=3000, speed_sp=500)
+
     if color_sensor.value == 5:
-        print 'OFF'
+        ev3.Sound.speak('Au Revoir')
+
+    else:
+        print(colors[cl.value()])
+        Sound.speak(colors[cl.value()])
