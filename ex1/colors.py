4d2c3f7f4d6c23bef901b697d5a9d59dae853a31
Crazy_Cactus <Mathias.Genesis@Gmail.com>
2017-03-06 14:54:47 +0100

    run motor and color test

--- ex1/colors.py
+++ ex1/colors.py
@@ -5,5 +5,8 @@ color_sensor.mode = 'COL-COLOR'
 
 while True:
     print(color_sensor.value())
+    motor = ev3.LargeMotor('outA')
+    motor.run_timed(time_sp=3000, speed_sp=500)
+    
     if color_sensor.value == 5:
         print 'OFF'
