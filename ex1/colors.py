095bb7ba7982c99b942440ff6b62e529064dfd19
Crazy_Cactus <Mathias.Genesis@Gmail.com>
2017-03-07 10:41:15 +0100

    debut ex2 et colors jours 2

--- ex1/colors.py
+++ ex1/colors.py
@@ -1,20 +1,21 @@
+import ev3dev.ev3 as ev3
 from ev3dev.ev3 import *
 
-ev3.Sound.setLanguage("French")
+
 
 color_sensor = ev3.ColorSensor()
 color_sensor.mode = 'COL-COLOR'
 
-colors = ["What the fuck", "noir", "bleu", "vert", "jaune", "alalalal", "blanc", "marron"]
+colors = ["What the fuck", "noir", "bluh", "ver", "zhaon", "alalalal", "blahn", "mahron"]
 
 while True:
-    print(color_sensor.value())
-    #motor = ev3.LargeMotor('outA')
-    #motor.run_timed(time_sp=3000, speed_sp=500)
+    print(colors[color_sensor.value()])
 
-    if color_sensor.value == 5:
-        ev3.Sound.speak('Au Revoir')
+
+    if color_sensor.value() == 5:
+        ev3.Sound.speak('oh revwar')
+        pass
 
     else:
-        print(colors[cl.value()])
-        Sound.speak(colors[cl.value()])
+        print(colors[color_sensor.value()])
+        Sound.speak(colors[color_sensor.value()]).wait()
